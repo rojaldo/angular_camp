d410bcfbf6c86cad2aa1d35f8e71979cc3b44d98
Daniel Rojo <danielrojo@gmail.com>
2021-02-16 15:25:08 +0100

    heroes compononent list and form created

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = ['Batman', 'Superman', 'Spiderman'];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addHero(tag){
+    console.log(tag.value);
+    this.heroes.push(tag.value);
+    tag.value = '';
   }
 
 }
