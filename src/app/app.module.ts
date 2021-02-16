01c024d620b70a4d44fc3da45a12aacd9d956d95
Joaquin <jdortega@indra.es>
2021-02-16 09:44:42 +0100

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { DisplayComponent } from './display/display.component';
+import { KeyboardComponent } from './keyboard/keyboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    DisplayComponent,
+    KeyboardComponent
   ],
   imports: [
     BrowserModule,
