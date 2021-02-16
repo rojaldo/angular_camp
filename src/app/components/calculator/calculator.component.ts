6272f964e1241dcf161431f36a18bf7492982482
Daniel Rojo <danielrojo@gmail.com>
2021-02-16 14:29:15 +0100

    Calculator service created and heroes compononent initialized

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,105 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CalculatorService } from 'src/app/services/calculator.service';
 
-enum states {INIT, FIRST_FIGURE, SECOND_FIGURE, RESULT}
 
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss']
 })
-export class CalculatorComponent implements OnInit {
+export class CalculatorComponent implements OnInit, OnDestroy {
 
   display = '';
-  currentState = states.INIT;
-  firstFigure = 0;
-  secondFigure = 0;
-  result = 0;
-  operator = '';
 
-  constructor() { }
+  constructor(public service: CalculatorService) { }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy()');
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit()');
+    this.display = this.service.getDisplay();
   }
 
   handleSignal(value: any): void {
     // this.result += value;
-    if(typeof value === 'string'){
-      this.handleSymbol(value);
-    } else if(typeof value === 'number') {
-      this.handleNumber(value);
-    }
-  }
-  handleNumber(value: number): void {
-    switch (this.currentState) {
-      case states.INIT:
-        this.firstFigure = value;
-        this.currentState = states.FIRST_FIGURE;
-        this.display = '' + value;
-        break;
-      case states.FIRST_FIGURE:
-        this.firstFigure = this.firstFigure * 10 + value;
-        this.display += value;
-        break;
-      case states.SECOND_FIGURE:
-        this.secondFigure = this.secondFigure * 10 + value;
-        this.display += value;
-        break;
-      case states.RESULT:
-        this.result = 0;
-        this.secondFigure = 0;
-        this.operator = '';
-        this.firstFigure = value;
-        this.currentState = states.FIRST_FIGURE;
-        this.display = '' + value;
-        break;
-      default:
-        break;
-    }
-  }
-
-  handleSymbol(value: string): void {
-    switch (this.currentState) {
-      case states.INIT:
-      break;
-      case states.FIRST_FIGURE:
-        if (value === '+' || value === '-' || value === '*' || value === '/'){
-          this.operator = value;
-          this.display += value;
-          this.currentState = states.SECOND_FIGURE;
-        }
-        break;
-      case states.SECOND_FIGURE:
-        if (value === '='){
-          this.result = this.resolve();
-          this.display = this.display + '=' + this.result;
-          this.currentState = states.RESULT;
-        }
-        break;
-      case states.RESULT:
-        this.secondFigure = 0;
-        this.operator = value;
-        this.firstFigure = this.result;
-        this.result = 0;
-        this.currentState = states.SECOND_FIGURE;
-        this.display = '' + this.firstFigure + this.operator;
-        break;
-      default:
-      break;
-    }
-  }
-
-  resolve(): number {
-    switch (this.operator) {
-      case '+':
-        return this.firstFigure + this.secondFigure;
-      case '-':
-        return this.firstFigure - this.secondFigure;
-      case '*':
-        return this.firstFigure * this.secondFigure;
-      case '/':
-        return this.firstFigure / this.secondFigure;
-      default:
-        break;
+    if (typeof value === 'string'){
+      this.display = this.service.handleSymbol(value);
+    } else if (typeof value === 'number') {
+      this.display = this.service.handleNumber(value);
     }
   }
 
