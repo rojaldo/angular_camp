80a7aebfcd5ba3ebd8a9f3289d62c24d2f1e4911
arapado <arapado@indra.es>
2021-02-18 15:23:21 +0100

    fixes

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,48 +1,79 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
 import { User } from 'src/app/model/user';
+import { FormServiceService } from 'src/app/form-service.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-
   user: User;
   formSubmitted = false;
 
+  countries = [];
+  search : any;
+  public model: any;
   fn = 'firstName';
   ln = 'lastName';
   e = 'email';
 
-  namePattern = '[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+';
-  emailPattern = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}';
+  namePattern = '[a-zA-ZñÑáéíóúÁÉÍÓÚs]+';
+  emailPattern = '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}';
 
   testForm = this.fb.group({
-    firstName: ['', [Validators.required, Validators.pattern(this.namePattern)]],
+    firstName: [
+      '',
+      [Validators.required, Validators.pattern(this.namePattern)],
+    ],
     lastName: ['', [Validators.required, Validators.pattern(this.namePattern)]],
     email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private formService: FormServiceService) {}
 
   ngOnInit(): void {
     this.user = new User('', '', '');
-
+    this.formService.getCountries().subscribe((data) => {
+      this.countries = this.processData(data);
+      console.log(this.countries);
+      this.search = (text$: Observable<string>) =>
+        text$.pipe(
+          debounceTime(200),
+          distinctUntilChanged(),
+          map((term) =>
+            term.length < 2
+              ? []
+              : this.countries
+                  .filter(
+                    (v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1
+                  )
+                  .slice(0, 10)
+          )
+        );
+    });
   }
 
   submitForm(form: any): void {
-       console.log('Form Data: ');
-       console.log(form);
-       // user.firstName = form.firstName
-   }
+    console.log('Form Data: ');
+    console.log(form);
+    // user.firstName = form.firstName
+  }
 
   isFieldValid(field: string): any {
     return (
-      this.testForm.get(field).errors && this.testForm.get(field).touched ||
-      this.testForm.get(field).untouched &&
-      this.formSubmitted && this.testForm.get(field).errors
+      (this.testForm.get(field).errors && this.testForm.get(field).touched) ||
+      (this.testForm.get(field).untouched &&
+        this.formSubmitted &&
+        this.testForm.get(field).errors)
     );
   }
 
@@ -55,4 +86,11 @@ export class FormComponent implements OnInit {
     }
   }
 
+  processData(data){
+    let array = []
+    data.forEach(element => {
+      array.push( element.name );
+    });
+    return array;
+  }
 }
---
+++ src/app/form-service.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormServiceService {
+
+  baseUrl = 'https://restcountries.eu/rest/v2/all';
+
+  constructor(public http: HttpClient) { }
+
+  getCountries(): Observable<any> {
+    return this.http.get(this.baseUrl);
+  }
+}
